35dff49349b4454b78fff9ef601a95250cf3c65c
pnthai285 <pnthai0000@gmail.com>
2025-08-23 00:39:53 +0700

    fix deviceid

--- include/common_types.h
+++ include/common_types.h
@@ -2,7 +2,7 @@
 #define COMMON_TYPES_H
 
 // Device-specific identifiers
-#define DEVICE_ID "esp32_health_sense"  // Device ID used for MQTT topic subscription
+#define DEVICE_ID "esp"  
 #define DEVICE_SECRET "ngotantai"
 #define USER_ID "default_user_id"
 
